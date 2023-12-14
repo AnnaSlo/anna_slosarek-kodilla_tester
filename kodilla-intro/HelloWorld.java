36b46f923cc44e333540378c99d54760d7fdfa4c
Anna Slosarek <anna.slosarek@gmail.com>
2023-12-14 23:22:19 +0100

    Hello World!

---
+++ kodilla-intro/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World" + "\n" + "Anna" + "\n" + 28 + "\n" + 'S');
+
+    }
+}
