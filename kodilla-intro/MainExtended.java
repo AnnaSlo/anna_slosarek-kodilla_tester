8bc2a812bc692af5e0a4ba8e2fff0e3e0f129957
Anna Slosarek <anna.slosarek@gmail.com>
2023-12-15 00:13:50 +0100

    MainExtended

---
+++ kodilla-intro/MainExtended.java
@@ -0,0 +1,14 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Anna";
+        String surname = "Slosarek";
+        int age = 28;
+        String adressStreetName = "Fiolkowa";
+        String adressStreetNumber = "54a";
+        double gradesAverage = 4.78;
+        char finalGrade = 'B';
+        boolean isGoodStudent = true;
+        System.out.println(name + "\n" + surname + "\n" + age + "\n" + adressStreetName + " " + adressStreetNumber + "\n" + gradesAverage + "\n" + finalGrade + "\n" + isGoodStudent);
+
+    }
+}
