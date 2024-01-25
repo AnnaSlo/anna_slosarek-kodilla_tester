3405968a2965fac03b21f1e4c0d3c94890c5343b
Anna Slosarek <anna.slosarek@gmail.com>
2024-01-25 15:37:34 +0100

    Color

---
+++ kodilla-intro/src/main/java/Color.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Color {
+
+    public static void main(String[] args) {
+        char userSelection = getUserSelection();
+        String colorName = getColorName(userSelection);
+        System.out.println("Pełna nazwa koloru: " + colorName);
+    }
+
+    public static char getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first letter of the color: ");
+        return scanner.next().charAt(0);
+    }
+    public static String getColorName(char firstLetter) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = Character.toUpperCase(firstLetter);
+        return switch (letter) {
+            case 'R' -> "Red";
+            case 'G' -> "Green";
+            case 'B' -> "Blue";
+            case 'Y' -> "Yellow";
+            case 'W' -> "White";
+            default -> "Unknown color";
+        };
+    }
+}
